2742767471406ecc177ad88e3b0e92184bd1785f
liurongxuan <liurongxuan@outlook.com>
2020-03-30 23:21:27 +0800

    Update app.js
    修改端口号

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ module.exports = app;
 // })
 
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
  
 var host = server.address().address
 var port = server.address().port
